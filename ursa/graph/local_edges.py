cb833e5467fa30131adbfa23ca27e5f90c88a62d
Omkar Salpekar <osalpekar@berkeley.edu>
2018-02-07 13:36:41 -0800

    Initial code for remote duplicate filtering functions

--- ursa/graph/local_edges.py
+++ ursa/graph/local_edges.py
@@ -44,6 +44,8 @@ class LocalEdges(object):
                        if type(val) is not ray.local_scheduler.ObjectID])
 
         if len(temp_buf) > MAX_SUBLIST_SIZE:
+            self.prune_buffer()
+            self.buf.sort()
             temp_edges.append(ray.put(self.buf))
             temp_buf = np.array([])
 
@@ -63,3 +65,66 @@ class LocalEdges(object):
         new_edges.extend(new_buf)
 
         return new_edges
+
+    def prune_buffer(self):
+        temp_set = set()
+        temp_buf = np.array([])
+
+        for edge in self.buf:
+            if edge in temp_set:
+                continue
+            temp_set.add(edge)
+            temp_buf.append(edge)
+
+        self.buf = temp_buf
+        temp_set = None
+
+    def prune_edges(self):
+        temp_set = set()
+        temp_edge_list = np.array([])
+
+        for list_oid in self.edges:
+            for edge in ray.get(list_oid):
+                if edge in temp_set:
+                    continue
+                temp_set.add(edge)
+                temp_edge_list.append(edge)
+
+        self.edges = np.array([])
+        for i in xrange(0, len(temp_edge_list), MAX_SUBLIST_SIZE):
+            x_oid = ray.put(list[i:i + MAX_SUBLIST_SIZE])
+            self.edges.append(x_oid)
+
+        temp_set = None
+        temp_edges_list = None
+
+    @ray.remote
+    def get_global_sampling(self):
+        sample_list = np.array([])
+        num_sublist_samples = 0.01 * MAX_SUBLIST_SIZE
+        for list_oid in self.edges:
+            np.append(sample_list,
+                      np.random.choice(list_oid, num_sublist_samples))
+        sample_list.sort()
+        return sample_list
+
+    @ray.remote
+    def get_partitions(self):
+        indices = [i*(0.01 * MAX_SUBLIST_SIZE * len(self.edges)) for i in range(1,len(self.edges))]
+        partitions_list = []
+        global_sample_list_oid = self.get_global_sampling
+        return [global_sample_list_oid[i] for i in indices]
+
+    @ray.remote
+    def partition_sublists(self):
+
+        partion_bounds = self.get_partitions()
+        
+        # case statement to compare each value in the list to the bound
+        # at this point you have list of sublists
+        # return that list of sublists
+
+    @ray.remote
+    def merge_common_partitions(self):
+        for list_oid in self.local_edges:
+            partitions_oid = partition_sublists()
