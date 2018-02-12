6855c7a3d920f0a7baf7df204a4ad270212e8dc3
Omkar Salpekar <osalpekar@berkeley.edu>
2018-02-12 02:02:58 -0800

    initial code for edge filtering

--- ursa/graph/local_edges.py
+++ ursa/graph/local_edges.py
@@ -52,6 +52,8 @@ class LocalEdges(object):
                        if type(val) is not ray.local_scheduler.ObjectID])
 
         if len(temp_buf) > MAX_SUBLIST_SIZE:
+            temp_buf = self.prune_buffer(temp_buf)
+            temp_buf.sort()
             temp_edges.append(ray.put(temp_buf))
             temp_buf = np.array([])
 
@@ -71,3 +73,75 @@ class LocalEdges(object):
         new_edges.extend(new_buf)
 
         return new_edges
+
+    def prune_buffer(self, input_buffer):
+        temp_set = set()
+        temp_buf = np.array([])
+
+        for edge in input_buffer:
+            if edge in temp_set:
+                continue
+            temp_set.add(edge)
+            temp_buf.append(edge)
+
+        temp_set = None
+        return temp_buf
+
+    @ray.remote
+    def get_global_sampling(self):
+        sample_list = np.array([])
+        num_sublist_samples = 0.01 * MAX_SUBLIST_SIZE
+        for list_oid in self.edges:
+            sample_list = np.append(sample_list,
+                                    np.random.choice(list_oid, 
+                                                     num_sublist_samples))
+        sample_list.sort()
+        return sample_list
+
+    @ray.remote
+    def get_partitions(self):
+        indices = [i*(0.01 * MAX_SUBLIST_SIZE)
+                   for i in range(1, len(self.edges))]
+        global_sample_list_oid = self.get_global_sampling
+        return [global_sample_list_oid[i] for i in indices]
+
+    @ray.remote
+    def partition_sublists(self, list_oid):
+        partion_bounds = self.get_partitions()
+        partitioned_sublist = [[] for i in range(len(partitions_bounds + 1))]
+        for edge in list_oid:
+            if edge.destination <= partition_bounds[0]:
+                partitioned_sublist[0].append(edge)
+            if edge.destination > partition_bounds[-1]:
+                partitioned_sublist[-1].append(edge)
+            for i in range(1, len(partition_bounds) - 1):
+                if edge.destination > partition_bounds[i] and
+                   edge.destination <= partition_bounds[i + 1]:
+                    paritioned_sublist[i].append(edge)
+        partition_oids = np.array([])
+        for sublist in partitioned_sublist:
+            partition_oids = np.append(partition_oids,
+                                       ray.put(sublist))
+        return partition_oids
+
+        # case statement to compare each value in the list to the bound
+        # at this point you have list of sublists
+        # return that list of sublists
+
+    @ray.remote
+    def merge_common_partitions(self):
+        # TODO: How to send this function to a remote object - is it just pass
+        # oid as argument?
+        merged_oid_groupings = []
+        new_local_edges = np.array([])
+        for list_oid in self.local_edges:
+            merged_oid_groupings.append(self.partition_sublists.remote(list_oid))
+
+        for i in range(len(merged_oid_groupings[0])):
+            new_partition = np.array([])
+            for j in range(len(merged_oid_groupings)):
+                new_partition = np.append(new_partition,
+                                          ray.get(merged_oid_groupings[j][i]))
+                new_partition_oid = ray.put(new_partition)
+                np.append(new_local_edges, new_partition_oid)
+        self.edges = new_local_edges
