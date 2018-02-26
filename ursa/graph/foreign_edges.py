2b3ea12b6e83dfb9e85bf686e383702329862241
Omkar Salpekar <osalpekar@berkeley.edu>
2018-02-25 22:48:02 -0800

    initial code for foreign edge buffering

---
+++ ursa/graph/foreign_edges.py
@@ -0,0 +1,17 @@
+import ray
+import numpy as np
+from .utils import _filter_remote
+
+MAX_SUBLIST_SIZE = 10
+
+class ForeignEdges(object):
+
+
+    def __init__(self, edges):
+        #takes in dict
+
+    def append(self, values):
+
+    def filter(self, filterfn):
+
+
